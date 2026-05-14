e0d875d70e37b1a972c8a864788959c3c952c540
pkvs1503 <pkvs1503@gmail.com>
2026-05-14 17:24:43 +0530

    debuged a small syntax error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,11 +7,9 @@ function App() {
     <>
     <Text display="Hello Movie"/>
     <Text display="Welcome to the Movie App"/>
-    {movieNumber == 1 ? (
+    {movieNumber == 1 && 
     <moviecard movie={{title: "Inception", description: "A mind-bending thriller about dreams within dreams.", poster: "https://m.media-amazon.com/images/I/51s+qj8n9lL._AC_.jpg"}}/>)
-    :(
-    <moviecard movie={{title: "The Matrix", description: "A hacker discovers the true nature of reality and his role in the war against its controllers.", poster: "https://m.media-amazon.com/images/I/51EG732BV3L._AC_.jpg"}}/>
-    )}
+    }   
     </>
   );
 }
