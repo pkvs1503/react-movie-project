cd7874a9481fc882907ec2ff67c3aa7191597262
pkvs1503 <pkvs1503@gmail.com>
2026-05-14 18:29:30 +0530

    now in order to implement where to which file to go when something is done ie path on path we need to implement router for that so need to install router from npm packages online ie to our project and implement that code router package functionalities and feautures in our project, so i installed react-router-dom package and we need to setup route code in our app.jsx file which is starting first file and use <main> tag inside main tag we need to use <routes> tag there inside routes tag we can provide n number of routes the first route will always be for path= / ie first route of any website any domain and we need specify which element ie which jsx file ie which file should render on that path actually

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,18 @@
 import './App.css'
-import moviecard from './components/moviecard';
+import favourites from './pages/favourites';
+import Home from './pages/home';
 
 function App() {
-  const movieNumber=1;
+ 
   return (
-    <>
-    <Text display="Hello Movie"/>
-    <Text display="Welcome to the Movie App"/>
-    {movieNumber == 1 && <moviecard movie={{title: "Inception", description: "A mind-bending thriller about dreams within dreams.", poster: "https://m.media-amazon.com/images/I/51s+qj8n9lL._AC_.jpg"}}/>}
-    </>
+    <main className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favourites" element={<favourites />} />
+      </Routes>
+    </main>
   );
 }
 
-function Text({ display }) {
-  return (
-    <div>
-      <p>{display}</p>
-    </div>
-  );
-}
 
 export default App;
---
+++ frontend/src/pages/favourites.jsx
@@ -0,0 +1,10 @@
+function favourites(){
+    return(
+        <div>
+            <h1>Favourites Page</h1>
+            <p>This is where your favourite movies will be displayed.</p>
+        </div>
+    );
+}
+
+export default favourites;
