b9b4913d86311879ca20c0d74d97c866583f6942
pkvs1503 <pkvs1503@gmail.com>
2026-05-14 17:07:22 +0530

    now added some embeddings of my contents using html tags and div into my component divided them as div containers added button in container to give it style and movie title and description in another container to give it style custom later on and put it all in a container called movie poster which is actually inside another container called main parent container called movie-poster this is how i structure my content for styling purpose later on

--- frontend/src/components/moviecard.jsx
+++ frontend/src/components/moviecard.jsx
@@ -1,9 +1,23 @@
 function moviecard({movie}) {
+
+    function onfavouriteClick(){
+        alert(`You have added ${movie.title} to your favourites!`);
+    }
     return(
         <div className="movie-card">
             <div className="movie-poster">
                 <img src={movie.poster} alt={movie.title}/>
+                <div className="movie-overlay">
+                  <button className="favourite-btn" onClick={onfavouriteClick}><Favourite>
+                    Favourite
+                    </button>
+                </div>
+                <div className="movie-info">
+                    <h3>{movie.title}</h3>
+                    <p>{movie.description}</p>
+                </div>
             </div>
            </div>
     );
 }
+
