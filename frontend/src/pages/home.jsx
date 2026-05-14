e4ec02c8c7bd50e8a44c9adff9f435d090c7c890
pkvs1503 <pkvs1503@gmail.com>
2026-05-14 20:05:33 +0530

    now i am using a new feauture here since simply calling it as normal way ie function will be called every time when component changes so idarinda resources pade pade hit aguthe pade pade request aguthe thumba resource response hagu n/w overload kuda agbahudu adu bahala tondare janaka adarinda adana hange balsuvudilla athava kareyuvudilla, ie now thats not ideal because we dont want to constantly be fecthing movie every single time when really nothing has changed in original data actually, theres a lot of times where we want to do something only once right when this component is rendered on screen , any uodates happened to component we dont want to actually be run this operation again

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,9 +1,24 @@
 import moviecard from '../components/moviecard';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+import {searchMovies, getPopularMovies} from '../services/api';
 import '.css/home.css';
 
 function Home() {
     const [searchQuery, setSearchQuery] = useState("");
+    const [popularMovies, setPopularMovies] = useState([]); // this is the state now which has data as array format
+    const [searchResults, setSearchResults] = useState([]);
+
+    useEffect(() => {
+        const loadPopularMovies = async () => {
+            try{
+                const movies = await getPopularMovies();
+                setPopularMovies(movies);
+            } catch (error) {
+                console.error("Error fetching popular movies:", error);
+            }
+        };
+        loadPopularMovies();
+    }, [] // when something changes in the dependency array, useEffect will run again. If we want to run it only once when the component mounts, we can pass an empty array.);
 
     const movies=[
         {title: "Inception", description: "A mind-bending thriller about dreams within dreams.", poster: "https://m.media-amazon.com/images/I/51s+qj8n9lL._AC_.jpg"},
