b277ea901dca22205811df58c0707428934d9ae3
pkvs1503 <pkvs1503@gmail.com>
2026-05-14 18:40:31 +0530

    now i created a simple navbar component, navbar basically is no where related to react router dom the main difference between react router dom and a navbar is in react router dom its a must thing for every react app and main thing which works on url path ie outside mostly and navbar is just a collection of links to navigate inside the app and not on url path mainly ie outside like its just a collection of <link> tag thats it and nothing else which are used to navigate quickly from one place to another place a quick shortcut we can say ie inside the application and not like outside which like react router dom which works main on url path and is an indispendable part and an integral must part of a react app of every react application

---
+++ frontend/src/pages/NavBar.jsx
@@ -0,0 +1,17 @@
+import {Link} from 'react-router-dom';
+
+function NavBar() {
+    return(
+        <nav className="navbar">
+           <div className="navbar-brand">
+            <Link to="/" className="navbar-logo">Movie App</Link>
+           </div>
+           <div className="navbar-links">
+            <Link to="/" className="navbar-link">Home</Link>
+            <Link to="/favourites" className="navbar-link">Favourites</Link>
+           </div>
+        </nav>
+    );
+}
+
+export default NavBar;
