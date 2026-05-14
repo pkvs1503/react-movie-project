6316e1a89eecff3f84c0f1418ae5de4f412493b1
pkvs1503 <pkvs1503@gmail.com>
2026-05-14 17:20:34 +0530

    there is another way also to do it ie we can use another operator in it ie && and this comes from concept called short circuiting in javascript

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,16 @@ import './App.css'
 import moviecard from './components/moviecard';
 
 function App() {
+  const movieNumber=1;
   return (
     <>
     <Text display="Hello Movie"/>
     <Text display="Welcome to the Movie App"/>
-    <moviecard movie={{title: "Inception", description: "A mind-bending thriller about dreams within dreams.", poster: "https://m.media-amazon.com/images/I/51s+qj8n9lL._AC_.jpg"}}/>
+    {movieNumber == 1 ? (
+    <moviecard movie={{title: "Inception", description: "A mind-bending thriller about dreams within dreams.", poster: "https://m.media-amazon.com/images/I/51s+qj8n9lL._AC_.jpg"}}/>)
+    :(
     <moviecard movie={{title: "The Matrix", description: "A hacker discovers the true nature of reality and his role in the war against its controllers.", poster: "https://m.media-amazon.com/images/I/51EG732BV3L._AC_.jpg"}}/>
+    )}
     </>
   );
 }
