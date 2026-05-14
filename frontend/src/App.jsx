754f71529964f5cd8777fef71b3c866c9e110f98
pkvs1503 <pkvs1503@gmail.com>
2026-05-14 18:44:20 +0530

    now i added embedded imported my navbar component inside my app.jsx file ie kind a first file first thing to display but first to render is main.jsx file so but component i used in app.jsx file because i need it in first display itself so then, navbar is also can be said a internal quick tour kind also ie when i click something inside the application i should quickly go to that displayed content for that purpose we use navbar basically

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,15 @@ import Home from './pages/home';
 function App() {
  
   return (
+    <div>
+    <Navbar/>
     <main className="App">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/favourites" element={<favourites />} />
       </Routes>
     </main>
+    </div>
   );
 }
 
